2293ad35063358dc9bda219bd9c37781f9b191ab
r21nomi <ryotakosu@gmail.com>
2017-08-24 21:28:22 +0900

    Show shader preview image

---
+++ mobile/src/main/java/com/r21nomi/arto/AppGlideModule.kt
@@ -0,0 +1,11 @@
+package com.r21nomi.arto
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by r21nomi on 2017/08/24.
+ */
+@GlideModule
+class AppGlideModule : AppGlideModule() {
+}
